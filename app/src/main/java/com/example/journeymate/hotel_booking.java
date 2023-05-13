49220b3ad559012e8c31df410ca3e78af2c258f0
isna syuhada <isnaanuar12@gmail.com>
2023-05-13 21:29:59 +0800

    Initial commit

--- app/src/main/java/com/example/journeymate/hotel_booking.java
+++ app/src/main/java/com/example/journeymate/hotel_booking.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class hotel_booking extends AppCompatActivity {
+public class activity_hotel_booking extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
